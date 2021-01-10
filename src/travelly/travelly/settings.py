f1a2a519a4df7fe2acf34b9a0c4d25924095e72d
Udemezue John <johnzeus14@gmail.com>
2021-01-10 11:22:25 +0100

    updated requiremnts and added env

--- src/travelly/travelly/settings.py
+++ src/travelly/travelly/settings.py
@@ -10,20 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-
+from dotenv import load_dotenv
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k^r4s@u@*#o(&3o#r5a^gz5l^=$^uga^rr#%b9wqt43axlj#x*'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
